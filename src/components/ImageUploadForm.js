641ed651845f608864ad60cd3810b00cbab84852
Naman82 <b520031@iiit-bh.ac.in>
2023-10-01 10:57:21 +0530

    prod changes

--- src/components/ImageUploadForm.js
+++ src/components/ImageUploadForm.js
@@ -35,7 +35,7 @@ function ImageUploadForm() {
 
     try {
       setLoading(true);
-      const response = await axios.post("http://127.0.0.1:8000/api/upload/", formData, {
+      const response = await axios.post("https://strip-spectrum-backend.onrender.com/api/upload/", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
